a70b2ad7032116f98591b2f006e18f70d219317c
lucas <lucas@Lucas>
2017-01-11 22:12:48 -0200

    Adicionando view engine "ejs" .

--- pradella-estudos-contatooh/config/express.js
+++ pradella-estudos-contatooh/config/express.js
@@ -1,10 +1,18 @@
 // config/express.js
 var express = require('express');
+
 module.exports = function() {
-var app = express();
-// configuração de ambiente
-app.set('port', 3000);
-// middleware
-app.use(express.static('./public'));
-return app;
+	var app = express();
+
+	// configuração de ambiente
+	app.set('port', 3000);
+
+	// middleware
+	app.use(express.static('./public'));
+
+	//configuracao ejs(template)
+	app.set('view engine','ejs')
+	app.set('views','./app/views')
+	
+	return app;
 };
