c5015b35217301d2c8cb9feb98d07facd95e2c53
lucas <lucas.pradella@hotmail.com>
2017-01-14 14:22:01 -0200

    Adicionando metodo para retornar epenas um contato .

--- pradella-estudos-contatooh/app/controllers/contato.js
+++ pradella-estudos-contatooh/app/controllers/contato.js
@@ -3,9 +3,9 @@
 // valos adicionar uma lista em memoria, logo vamos refatorar para buscar essas informacoes de uma base . 
  var contatos= [
 	 {_id : 1, nome : "java", email : "java@pradella.com.br"},
-	 {_id : 1, nome : "scala", email : "scalaa@pradella.com.br"},
-	 {_id : 1, nome : "nodejs", email : "nodejs@pradella.com.br"},
-	 {_id : 1, nome : "javaScript", email : "JavaScript@pradella.com.br"}
+	 {_id : 2, nome : "scala", email : "scalaa@pradella.com.br"},
+	 {_id : 3, nome : "nodejs", email : "nodejs@pradella.com.br"},
+	 {_id : 4, nome : "javaScript", email : "JavaScript@pradella.com.br"}
  ]
 
 
@@ -14,6 +14,19 @@ module.exports = function(){
 	controller.listaContatos = function(req, res){
 		res.json(contatos);
 	};
-	return controller;
 
-}
+	
+	controller.obtemContato = function(req, res){
+		console.log(req.params.id);
+
+		var idContato = req.params.id;
+		var contato = contatos.filter(function(contato){
+			return contato._id == idContato;
+		})[0];
+		
+		contato ? 
+			res.json(contato): res.status(404).send('contato nao encontrado');
+		};
+		
+	return controller;
+  };
--- pradella-estudos-contatooh/app/routes/contato.js
+++ pradella-estudos-contatooh/app/routes/contato.js
@@ -2,5 +2,6 @@
 module.exports = function(app){
 	var controller = app.controllers.contato;
 	app.get('/contatos', controller.listaContatos);
+	app.get('/contatos/:id', controller.obtemContato);
 
 }
