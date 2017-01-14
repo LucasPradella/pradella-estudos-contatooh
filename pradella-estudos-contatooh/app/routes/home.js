6a1eaa53b14de860a34422a97b5325aca9ee0a6c
lucas <lucas.pradella@hotmail.com>
2017-01-14 13:13:54 -0200

    alteracao router express-load

--- pradella-estudos-contatooh/app/routes/home.js
+++ pradella-estudos-contatooh/app/routes/home.js
@@ -1,9 +1,7 @@
 //Rotas
-
-//quando usamos express load essa linha nao e mais necessaria
-// var controller = require('../controllers/home')();
+var controller = require('../controllers/home')();
 
 module.exports = function(app){
-	var controller = app.controllers.home;
 	app.get('/', controller.index);
+	app.get('/index', controller.index);
 }
