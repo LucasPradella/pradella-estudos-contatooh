d45ed5c8af81707d99353f56be2a5636431423f2
lucas <lucas.pradella@hotmail.com>
2017-01-25 19:45:43 -0200

    Adicionando diretiva ng-repeat

--- pradella-estudos-contatooh/public/js/controllers/ContatosController.js
+++ pradella-estudos-contatooh/public/js/controllers/ContatosController.js
@@ -4,5 +4,24 @@ angular.module('contatooh').controller('ContatosController',
 			$scope.incrementa = function(){
 				$scope.total++;
 			}
+			
+			
+			$scope.contatos = [
+				{
+			    	"_id":1,
+				    "nome":"Contato Java",
+				    "email":"java@email.com.br"
+				},
+				{
+			    	"_id":2,
+				    "nome":"Contato scala",
+				    "email":"scala@email.com.br"
+				},
+				{
+			    	"_id":3,
+				    "nome":"Contato JavaScript",
+				    "email":"javaScript@email.com.br"
+				}
+			]
 	
 });
