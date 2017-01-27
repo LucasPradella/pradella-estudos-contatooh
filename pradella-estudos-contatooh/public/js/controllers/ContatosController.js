4bde0408f0debaf0a6da270aca8ca303da6d9fc8
lucas <lucas.pradella@hotmail.com>
2017-01-26 22:29:25 -0200

    Refatorando e consumindo rest EndPoints com recource

--- pradella-estudos-contatooh/public/js/controllers/ContatosController.js
+++ pradella-estudos-contatooh/public/js/controllers/ContatosController.js
@@ -1,22 +1,27 @@
 angular.module('contatooh').controller('ContatosController',
-		function($scope, $http) {
+		function($scope, $resource) {
 
 			$scope.contatos = [];
 
-			$scope.total = 0;
-
-			$scope.filtro = '';
-
+/*			$scope.total = 0;
 			$scope.incrementa = function() {
 				$scope.total++;
 			};
+*/
+			$scope.filtro = '';
 
-			$http.get('/contatos')
-				.then(function(data) {
-					$scope.contatos = data;
-				}),function(result) {
-					console.log("Nao foi possivel obter a lista de contatos");
-					console.log(result);
-			};
+
+			var Contato = $resource('/contatos/:id');
+		
+			function buscaContatos(){
+					Contato.query(function(contatos) {
+						$scope.contatos = contatos;
+					}, function(erro) {
+						console.log("Nao foi possivel obter a listar de contatos");
+						console.log(erro);
+					});
+			} 
+			buscaContatos();
 			
+
 		});
