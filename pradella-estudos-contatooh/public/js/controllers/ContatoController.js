0c1b22b0097b62dfda67e72270fd05fa07bff7c6
lucas <lucas.pradella@hotmail.com>
2017-04-26 17:43:20 -0300

    Correção contato controller.

--- pradella-estudos-contatooh/public/js/controllers/ContatoController.js
+++ pradella-estudos-contatooh/public/js/controllers/ContatoController.js
@@ -1,9 +1,48 @@
 angular.module('contatooh').controller('ContatoController',
-		function($scope, $routeParams, Contato) {
-			console.log($routeParams.contatoId);
+				function($scope,Contato,$routeParams)  {
+
+					if($routeParams.contatoId) {
+						Contato.get({id: $routeParams.contatoId}, 
+							function(contato) {
+								$scope.contato = contato;
+							}, 
+							function(erro) {
+								$scope.mensagem = {
+								  texto: 'Contato não existe. Novo contato.'
+								};
+							}
+						);	
+					} else {
+						$scope.contato = new Contato();
+					}
+
+
+					$scope.salva = function() {
+					  $scope.contato.$save()
+					  	.then(function() {
+					  		$scope.mensagem = {texto: 'Salvo com sucesso'};
+					  		// limpa o formulário
+					  		$scope.contato = new Contato();
+					  	})
+					  	.catch(function(erro) {
+					  		$scope.mensagem = {texto: 'Não foi possível salvar'};
+					  	});
+					};	
+
+					Contato.query(function(contatos) {
+						$scope.contatos = contatos;
+			    	});	
+			});
+
+
+// versão antiga antes de ligar o mongodb
+/*{
+			
+	
+	       console.log($routeParams.contatoId);
 
 			
-/*			var Contato = $resource('/contatos/:id'); deletado quando iniciamos a utilizacao do contatoService*/
+			var Contato = $resource('/contatos/:id'); deletado quando iniciamos a utilizacao do contatoService
 
 			Contato.get({id : $routeParams.contatoId}, function(contato) {
 				$scope.contato = contato;
@@ -17,14 +56,14 @@ angular.module('contatooh').controller('ContatoController',
 			
 			$scope.salva = function(){
 				$scope.contato.$save()
-					.then(function(){
-						$scope.mensagem = {texto:"Salvo com sucesso"}
-						
-						$scope.contato = new Contato();
-					})
-					.catch(function(erro){
-						$scope.mensagem = {texto:"nao foi possivel salvar"}
-					});
+					.then(function() {
+							$scope.mensagem = {texto: 'Salvo com sucesso'};
+							$scope.contato = new Contato();
+						})
+						.catch(function(erro) {
+							$scope.mensagem = {texto: 'Não foi possível salvar'};
+						});
 			};
 			
 		});
+*/
